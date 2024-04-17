5471ae9a8dd35308ff40394634f7a74c03aba287
siegbk <siegbk@gmail.com>
2024-04-17 15:15:28 +0900

    크롤링 연습 2

--- Chap09_클리앙중고장터검색.py
+++ Chap09_클리앙중고장터검색.py
@@ -1,10 +1,13 @@
 #Chap09_클리앙중고장터검색.py
 # coding:utf-8
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup #정적인 사이트에 강하다.
+#python built lib
 import urllib.request
+#regular expression
 import re 
 
-for n in range(0,10):
+f = open("cl.txt", "wt", encoding="utf-8")
+for n in range(0,3):
         #클리앙의 중고장터 주소 
         url ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
         print(url)
@@ -16,6 +19,13 @@ for n in range(0,10):
 		# </span>
         for item in list:
             title = item.text.strip()
-            print(title)
+            #print(title)
+            #키워드 검색
+            if re.search("아이폰", title):
+                  print(title)
+                  f.write(f"{title}\n")
+
+f.close()
 
 
+#https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=1
